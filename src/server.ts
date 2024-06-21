b7bd51f452f41333dae495fb45bf96f1e1f73424
Makdoom Shaikh <makshaikh99@gmail.com>
2024-06-21 23:40:54 +0530

    feat: first build

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,6 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 app.listen(port, async () => {
+  console.log("testing");
   Logger.info(`🚀 Server up and running at http://localhost:${port}`);
 });
